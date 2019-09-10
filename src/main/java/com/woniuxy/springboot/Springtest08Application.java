ec188f656d93ee719958a103ad6cee03ad0ab75e
a <a@163.com>
2019-09-10 17:18:02 +0800

    init

---
+++ src/main/java/com/woniuxy/springboot/Springtest08Application.java
@@ -0,0 +1,13 @@
+package com.woniuxy.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtest08Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtest08Application.class, args);
+	}
+
+}
